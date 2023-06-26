25c38e49f69c20b3b9d6ca8473c9eab19af359df
Alessio <belcastroalessio@outlook.it>
2023-06-26 03:01:02 +0200

    EPICODE M2 D2

---
+++ S2P1-scaricabile.js
@@ -0,0 +1,194 @@
+//ESERCIZI SUGLI IF: 
+
+/* ESERCIZIO 1
+ Scrivi un algoritmo per trovare il più grande tra due numeri interi.
+*/
+
+let num1 = 5
+let num2 = 8
+
+console.log("--------------------------------------");
+console.log("Esercizio 1: \n");
+
+if(num1>num2){
+  console.log("NUM1: ",num1 ," è più grande di NUM2: ", num2);
+}else{ console.log("NUM2: ",num2 ," è più grande di NUM1: ", num1); }
+
+console.log("-------------------");
+
+/*
+ESERCIZIO 2
+  Crea un blocco condizionale if/else per mostrare in console il messaggio corretto in ogni condizione.
+
+  num < 5 - mostra in console "Tiny"
+  num < 10 - mostra in console "Small"
+  num < 15 - mostra in console "Medium"
+  num < 20 - mostra in console "Large"
+  num >= 20 - mostra in console "Huge"
+*/
+let num = 12
+
+console.log("Esercizio 2: \n");
+
+if(num<5){
+  console.log("Tiny");
+}
+else if(num<10 && num>5){
+  console.log("Small");
+}
+else if(num<15 && num>10){
+  console.log("Medium");
+}
+else if(num<20 && num>15){
+  console.log("Large");
+}
+else{
+  console.log("Huge\n");
+}
+
+console.log("-------------------");
+
+//ESERCIZI SUI CICLI: 
+
+/* ESERCIZIO 3
+  Mostra i numeri da 0 a 10 (incluso) in ordine ascendente, ma evitando di mostrare i numeri 3 e 8 (suggerimento: ripassa l'uso di "continue").
+*/
+
+console.log("Esercizio 3: \n");
+
+for (let i = 0; i <= 10; i++) {
+  if (i==3 || i==8) {
+    continue;
+  }
+  console.log(i);
+  
+}
+
+console.log("-------------------");
+
+/* ESERCIZIO 11
+  Scrivi un ciclo in JavaScript per iterare da 0 a 15. Per ciascun elemento, il ciclo deve controllare the il valore corrente sia pari o dispari, e mostrare il risultato in console.
+*/
+
+console.log("Esercizio 4: \n");
+
+for (let i = 0; i<=15; i++) {
+  if(i % 2 == 0){
+    console.log(i, "\tPari");
+  }else{
+    console.log(i, "\tDispari");
+  }
+}
+
+console.log("\n--------------------------------------\n");
+
+//ESERCIZI EXTRA NON OBBLIGATORI
+
+/* ESERCIZIO EXTRA 1
+  Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
+*/
+
+console.log("Esercizio EXTRA 1: \n");
+
+if(num1 == 8 || num2 == 8){
+    console.log("uno dei due numeri è 8");
+}else if(num1 + num2 == 8){
+  console.log("La somma dei due numeri è 8");
+}else if(num1 - num2 == 8){
+  console.log("La sottrazione dei due numeri è 8");
+}else{
+  console.log("Nessuna delle condizioni è soddisfatta");
+}
+
+console.log("-------------------");
+
+/* ESERCIZIO EXTRA 2
+Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
+C'è una promozione in corso: se il totale del carrello supera 50, l'utente ha diritto alla spedizione gratuita (altrimenti la spedizione ha un costo fisso pari a 10).
+Crea un algoritmo che determini l'ammontare totale che deve essere addebitato all'utente per il checkout.
+*/
+
+console.log("Esercizio EXTRA 2: \n");
+
+let totalShoppingCart = 45
+
+console.log("Totale Carrelo: ", totalShoppingCart);
+if(totalShoppingCart > 50){
+  console.log("Promo Riscattata, Prezzo finale: ", totalShoppingCart);
+}else{
+  console.log("Promo Negata, Prezzo finale: ", totalShoppingCart + 10);
+}
+
+console.log("-------------------");
+
+/* ESERCIZIO EXTRA 3
+  Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
+  Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando, usando l'algoritmo del codice precedente, se le spedizioni siano gratuite oppure no e e calcolando il totale.
+*/
+
+console.log("Esercizio EXTRA 3: \n");
+
+totalShoppingCart = totalShoppingCart - (totalShoppingCart/100)*20
+
+console.log("BLACK FRIDAY!, prezzo compreso di sconto: ", totalShoppingCart);
+
+if(totalShoppingCart > 50){
+  console.log("Promo Riscattata, Prezzo finale: ", totalShoppingCart);
+}else{
+  console.log("Promo Negata, Prezzo finale: ", totalShoppingCart + 10);
+}
+
+console.log("-------------------");
+
+/*  ESERCIZIO EXTRA 4
+  Usa un operatore ternaio per assegnare ad una variabile chiamata "gender" i valori "male" o "female".
+  La scelta deve essere basata sul valore di un'altra variabile booleana chiamata isMale.
+  Es. se isMale e' vero, il valore di gender deve essere "male"
+*/
+
+console.log("Esercizio EXTRA 4: \n");
+
+let isMale = false
+let gender
+
+if(isMale == true){
+  gender = "male"
+  console.log(gender);
+}else{
+  gender = "female"
+  console.log(gender);
+}
+
+console.log("-------------------");
+
+/* ESERCIZIO EXTRA 5
+  Scrivi un algoritmo che iteri i numeri da 1 a 100, stampandoli in console. Se un valore tuttavia è multiplo di 3 (operatore modulo!), stampa al suo posto la parola "Fizz" e se il numero è multiplo di 5, stampa "Buzz". Se le condizioni si verificano entrambe, stampa "FizzBuzz".
+*/
+
+console.log("Esercizio EXTRA 5: \n");
+
+for (let i = 0; i <= 100; i++) {
+
+  if(i % 3 == 0 && i % 5 == 0){
+
+    console.log("FizzBuzz");
+    continue;
+
+  }else if(i % 5 == 0){
+
+    console.log("Buzz");
+    continue;
+
+  }else if(i % 3 == 0){
+
+    console.log("Fizz");
+    continue;
+
+  }
+
+  console.log(i);
+  
+}
+
+console.log("-------------------");
+console.log("--------------------------------------");
